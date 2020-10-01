df34292b8075bd41579ed979be0c5b17cbc47676
Andrei Shelenhouski <andrei_shelenhouski@epam.com>
2020-10-01 21:44:38 +0300

    feat(module-5): implement middleware to log called methods

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,11 +1,24 @@
 import { NextFunction, Request, Response } from 'express';
+import winston from 'winston';
+
+const logger: winston.Logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console());
+}
 
 export const loggerMiddleware: (req: Request, resp: Response, next: NextFunction) => void = (
   req: Request,
   resp: Response,
   next: NextFunction,
 ) => {
-  // eslint-disable-next-line no-console
-  console.log('Request logged:', req.method, req.path);
+  logger.info(`${req.method}: ${req.path} - ${JSON.stringify(req.body)}`);
   next();
 };
